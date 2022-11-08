5aa6ee2504ead4a59711eb5c7d63247464a55510
Karthikeyan Balasubramanian <karthikeyan@Karthikeyans-MacBook-Pro.local>
2022-11-08 16:22:30 +0530

    Layout added

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <ul>
+          <li>Product 1</li>
+          <li>Product 2</li>
+        </ul>
+      </div>
+    </Layout>
+  )
+}
